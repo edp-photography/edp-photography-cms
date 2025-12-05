ed2fa4f73bea8510590e3e4ea7baa125194f63ac
tchappel <thomas.chappel4@gmail.com>
2025-12-05 12:03:10 +0100

    delete image gallery from homepage

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -483,8 +483,6 @@ export interface ApiHomePageHomePage extends Struct.SingleTypeSchema {
       Schema.Attribute.Private;
     heroGallery: Schema.Attribute.Component<'blocks.hero-gallery', false> &
       Schema.Attribute.Required;
-    imageGallery: Schema.Attribute.Component<'blocks.image-gallery', false> &
-      Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
