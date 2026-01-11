b2b9dfacc759fb0c46aa476b53f87e029ebb568d
tchappel <thomas.chappel4@gmail.com>
2026-01-11 13:50:12 +0100

    add Image Gallery in About Page

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -482,6 +482,7 @@ export interface ApiAboutPageAboutPage extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    imageGallery: Schema.Attribute.Component<'blocks.image-gallery', false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
